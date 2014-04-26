e6e72232bee45a010d20099b3339da6bb4de4e08
VEINHORN <mcklas@mail.ru>
2014-04-26 23:51:09 +0300

    Added some improvements to fetcher.

--- LostFilmTvAndroid/LostFilmTvAndroid/src/main/java/com/lostfilmtvandroid/SerialDescriptionLoader.java
+++ LostFilmTvAndroid/LostFilmTvAndroid/src/main/java/com/lostfilmtvandroid/SerialDescriptionLoader.java
@@ -10,6 +10,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by veinhorn on 26.4.14.
@@ -26,10 +28,17 @@ public class SerialDescriptionLoader extends AsyncTask<String, Integer, String> 
     @Override
     protected String doInBackground(String... params) {
         String poster = "";
-        String text = "";
-        //String country = ""; // don't forget
         String year = "";
         String genres = "";
+        String numberOfSeasons = "";
+        String description = "";
+
+        String rawDescription = ""; // contains raw text from div
+        String country = "";
+        String status = "";
+
+        String officialPage = "";
+
         try {
             Document document = Jsoup.connect(url).get();
             Elements elements = document.getElementsByClass("mid");
@@ -39,11 +48,27 @@ public class SerialDescriptionLoader extends AsyncTask<String, Integer, String> 
             Elements spanElements = serialDescriptionElement.getElementsByTag("span");
             year = spanElements.get(0).text();
             genres = spanElements.get(1).text();
-            //country = serialDescriptionElement.getElementsByTag("span").toString();
+            numberOfSeasons = spanElements.get(2).text();
+            description = spanElements.get(3).text();
+
+            rawDescription = serialDescriptionElement.ownText();
+            Pattern pattern = Pattern.compile("Страна: [a-zA-Zа-яА-Я]+\\b");
+            Matcher matcher = pattern.matcher(rawDescription);
+            while(matcher.find()) {
+                country = matcher.group().split(" ")[1];
+            }
+            pattern = Pattern.compile("Статус: [a-zA-Zа-яА-Я]+\\b");
+            matcher = pattern.matcher(rawDescription);
+            while(matcher.find()) {
+                status = matcher.group().split(" ")[1];
+            }
+
+            officialPage = serialDescriptionElement.getElementsByTag("a").get(0).attr("href");
+
         } catch(IOException e) {
             Log.e(StringConstants.TAG, StringConstants.EXCEPTION, e);
         }
-        return genres;
+        return officialPage;
     }
 
     @Override
