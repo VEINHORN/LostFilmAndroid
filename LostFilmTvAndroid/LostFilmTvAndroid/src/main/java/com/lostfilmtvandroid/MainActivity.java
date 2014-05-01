9c8f69ec14320828f1adda6eb4edd83a397ed464
VEINHORN <mcklas@mail.ru>
2014-05-02 02:19:36 +0300

    Design was updated.

--- LostFilmTvAndroid/LostFilmTvAndroid/src/main/java/com/lostfilmtvandroid/MainActivity.java
+++ LostFilmTvAndroid/LostFilmTvAndroid/src/main/java/com/lostfilmtvandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lostfilmtvandroid;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -8,19 +9,21 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.GridView;
 
 import com.lostfilmtvandroid.lostfilmtv.entities.SerialsContainer;
 import com.lostfilmtvandroid.serialslist.SerialsAdapter;
 import com.lostfilmtvandroid.serialslist.SerialsLoader;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 
 public class MainActivity extends ActionBarActivity {
     private SerialsLoader serialsLoader;
-    private ListView serialsListView;
+    //private ListView serialsListView;
+    private GridView gridView;
     private SerialsAdapter serialsAdapter;
-    private TextView testTextView;
     private EditText searchEditText;
 
     //private UITableView uiTableView;
@@ -28,7 +31,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault("fonts/Roboto-Light.ttf");
         setContentView(R.layout.activity_main);
+
         //uiTableView = (UITableView)findViewById(R.id.tableView);
 
         /*
@@ -46,7 +51,6 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });*/
-        testTextView = (TextView)findViewById(R.id.testtextview);
         LostFilmFromXML lostFilmFromXML = new LostFilmFromXML();
 
         searchEditText = (EditText)findViewById(R.id.search_edit_text);
@@ -67,10 +71,11 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        serialsListView = (ListView)findViewById(R.id.serials_list_view);
+        gridView = (GridView)findViewById(R.id.gridview);
+        //serialsListView = (ListView)findViewById(R.id.serials_list_view);
         serialsAdapter = new SerialsAdapter(this, new SerialsContainer());
-        serialsListView.setAdapter(serialsAdapter);
-        serialsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridView.setAdapter(serialsAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MainActivity.this, SerialDescriptionActivity.class);
@@ -81,5 +86,10 @@ public class MainActivity extends ActionBarActivity {
         serialsLoader = new SerialsLoader(this, serialsAdapter);
         serialsLoader.execute();
 
+    }
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(new CalligraphyContextWrapper(context, R.attr.fontPath));
     }
 }
