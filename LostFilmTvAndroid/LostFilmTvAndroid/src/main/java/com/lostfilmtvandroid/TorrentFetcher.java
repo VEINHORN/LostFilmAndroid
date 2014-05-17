a9fb4dde838386da4a3e9c258d880fbe194311ae
VEINHORN <mcklas@mail.ru>
2014-05-17 03:50:45 +0300

    Added torrent buttons.

--- LostFilmTvAndroid/LostFilmTvAndroid/src/main/java/com/lostfilmtvandroid/TorrentFetcher.java
+++ LostFilmTvAndroid/LostFilmTvAndroid/src/main/java/com/lostfilmtvandroid/TorrentFetcher.java
@@ -1,31 +1,34 @@
 package com.lostfilmtvandroid;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.AsyncTask;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lostfilmtvandroid.torrentrss.EpisodesContainer;
 import com.lostfilmtvandroid.torrentrss.TorrentRssFetcher;
+import com.qustom.dialog.QustomDialogBuilder;
 
 /**
  * Created by veinhorn on 11.5.14.
  */
 public class TorrentFetcher extends AsyncTask<String, String, EpisodesContainer> {
     private ProgressDialog progressDialog;
-    private Context context;
+    private Activity activity;
     private String serialTitle;
     private String episodeTitle;
 
-    public TorrentFetcher(Context context, String serialTitle, String episodeTitle) {
-        this.context = context;
+    public TorrentFetcher(Activity activity, String serialTitle, String episodeTitle) {
+        this.activity = activity;
         this.serialTitle = serialTitle;
         this.episodeTitle = episodeTitle;
     }
 
     @Override
     protected void onPreExecute() {
-        progressDialog = ProgressDialog.show(context, "Searching", "Searching torrent");
+        progressDialog = ProgressDialog.show(activity, "Searching", "Searching torrent");
     }
 
     @Override
@@ -37,9 +40,22 @@ public class TorrentFetcher extends AsyncTask<String, String, EpisodesContainer>
     protected void onPostExecute(EpisodesContainer episodesContainer) {
         progressDialog.hide();
         if(episodesContainer.isSuchEpisode(episodeTitle)) {
-            Toast.makeText(context, "true", Toast.LENGTH_SHORT).show();
+            QustomDialogBuilder qustomDialogBuilder = new QustomDialogBuilder(activity)
+                    .setTitle("Download torrent")
+                    .setMessage("Select the torrent quality and start downloading")
+                    .setCustomView(R.layout.torrent_buttons, activity)
+                    .setTitleColor("#FF00FF")
+                    .setDividerColor("#FF00FF");
+            qustomDialogBuilder.show();
+            TextView hqTorrentButton = (TextView)qustomDialogBuilder.getDialogView().findViewById(R.id.hq_torrent_button);
+            hqTorrentButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(activity, "Text", Toast.LENGTH_SHORT).show();
+                }
+            });
         } else {
-            Toast.makeText(context, "false", Toast.LENGTH_SHORT).show();
+            Toast.makeText(activity, "false", Toast.LENGTH_SHORT).show();
         }
     }
 }
